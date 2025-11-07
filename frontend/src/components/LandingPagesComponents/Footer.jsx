fb73e4fb261c615932ca99fd52889ceddfa75c66
abdelrahman962 <abdelrahmanmasri3@gmail.com>
2025-11-07 16:12:57 +0200

    Track all file changes

--- frontend/src/components/LandingPagesComponents/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-6 mt-10">
-      <p className="text-sm">
-        © {new Date().getFullYear()} Medical Lab System. All rights reserved.
-      </p>
-    </footer>
-  );
-}
